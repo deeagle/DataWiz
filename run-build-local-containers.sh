6352680286d22c9da63c6e1e15dee535e903913f
Martin deeagle Kock <mko@leibniz-psychology.org>
2025-02-20 14:33:44 +0100

    build(docker): load containers locally

--- run-build-local-containers.sh
+++ run-build-local-containers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker buildx build -f Spss-Api.Dockerfile -t "datawiz/spss-api:local" .
-docker buildx build -f Dockerfile -t "datawiz/datawiz:local" .
+docker buildx build -f Spss-Api.Dockerfile -t "datawiz/spss-api:local" --load .
+docker buildx build -f Dockerfile -t "datawiz/datawiz:local" --load .
